fcb09774c9a2d3ce9b4e773dd6ee39b7f18da926
Коновалова Настасья Олеговна <KNO.18@free.uni-dubna.ru>
2019-10-26 11:47:41 +0300

    rational nambers calculate_Konovalova

---
+++ calculator2/calculator2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator2
+{
+    public partial class Form1 : Form
+    {
+        public string currentOperator;
+        public double result;
+        public string d;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentOperator = "-";
+            label2.Text = Convert.ToString("-");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RationalNumber rat1 = new RationalNumber(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox4.Text));
+            RationalNumber rat2 = new RationalNumber(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox5.Text));
+
+            RationalNumber result = new RationalNumber(0, 1);
+            
+
+            switch (currentOperator)
+            {
+                case "-":
+                    result = rat1 - rat2;
+                    break;
+                case "+":
+                    result= rat1 + rat2;
+                    break;
+                case "*":
+                   result= rat1  *rat2;
+                    break;
+                case "/":
+                  result= rat1 / rat2;
+                    break;
+            }
+            textBox3.Text = Convert.ToString(result.c);
+            textBox6.Text = Convert.ToString(result.z);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentOperator = "*";
+            label2.Text = Convert.ToString("*");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentOperator = "+";
+            label2.Text = Convert.ToString("+");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentOperator = "/";
+            label2.Text = Convert.ToString("/");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentOperator = "0";
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ calculator2/calculator2/RationalNumber.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator2
+{
+    class RationalNumber
+    {
+
+        public double c = 0;
+        public double z = 0;
+
+        public RationalNumber(int c, int z)
+        {
+            this.c = c;
+            this.z = z;
+        }
+        //slozhenie
+        public static RationalNumber operator +(RationalNumber rat1, RationalNumber rat2)
+        {
+            RationalNumber result = new RationalNumber(1, 1);
+            result.c = (rat1.c * rat2.z + rat1.z * rat2.c);
+            result.z = rat1.z * rat2.z;
+
+            result.socr(result.c, result.z);
+
+            return result;
+        }
+        //sokrachenie
+        public void socr(double chis, double znam)
+        {
+            int nod = this.Nod(chis,znam);
+            this.c = c / nod;
+            this.z = z / nod;
+        }
+
+        //naimenshiy obschiy delitel
+        private int Nod(double chis, double znam)
+        {
+            while ((chis != 0) && (znam != 0))
+            {
+                if (chis > znam)
+                    chis -= znam;
+                else
+                    znam -= chis;
+            }
+
+            return Math.Max(Convert.ToInt32(chis), (Convert.ToInt32(znam)));
+        }
+
+
+        public static RationalNumber operator -(RationalNumber rat1, RationalNumber rat2)
+        {
+            RationalNumber result = new RationalNumber(1, 1);
+            result.c = (rat1.c * rat2.z - rat1.z * rat2.c);
+            result.z = rat1.z * rat2.z;
+
+            result.socr(result.c, result.z);
+
+            return result;
+        }
+
+        public static RationalNumber operator *(RationalNumber rat1, RationalNumber rat2)
+        {
+            RationalNumber result = new RationalNumber(1, 1);
+            result.c = (rat1.c * rat2.c);
+            result.z = rat1.z * rat2.z;
+
+            result.socr(result.c, result.z);
+
+            return result;
+        }
+
+        public static RationalNumber operator /(RationalNumber rat1, RationalNumber rat2)
+        {
+            RationalNumber result = new RationalNumber(1, 1);
+            result.c = (rat1.c / rat2.c);
+            result.z = rat1.z / rat2.z;
+
+            result.socr(result.c, result.z);
+
+            return result;
+        }
+
+
+
+        
+    }
+}
